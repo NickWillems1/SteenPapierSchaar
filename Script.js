36ede19fac46cfbbe81c9c49203db04ffe9b3538
NickWillems1 <nick.willems1@student.gildeopleidingen.nl>
2026-06-23 11:36:37 +0200

    Add initial HTML and JavaScript files for Steen Papier Schaar game

---
+++ Script.js
@@ -0,0 +1,5 @@
+const humanOutput = document.querySelector("#human");
+const computerKeuze = document.querySelector("#computer");
+const uitkomst = document.querySelector("result")
+
+humanOutput.innerHTML = "Jouw keuze komt hier, maak je keuze!";
