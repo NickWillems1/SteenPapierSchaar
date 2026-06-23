9a62311227e3761b93b9232d18ddc61918ce745b
NickWillems1 <nick.willems1@student.gildeopleidingen.nl>
2026-06-23 13:09:34 +0200

    Refactor game logic and streamline button click events

--- Script.js
+++ Script.js
@@ -1,26 +1,55 @@
+//constants and variables
+
 const humanOutput = document.querySelector("#human");
-const computerKeuze = document.querySelector("#computer");
-const uitkomst = document.querySelector("#result")
+const computerOutput = document.querySelector("#computer")
+const result = document.querySelector("#result")
 const steenBTN = document.querySelector("#steen")
 const papierBTN = document.querySelector("#papier")
 const schaarBTN = document.querySelector("#schaar")
-
 let humanChoice = " ";
-
+let computerChoice = "";
 humanOutput.innerHTML = "Jouw keuze komt hier, maak je keuze!";
 
+//define wintable
 
-// button click event
+const wins = {
+    steen: "schaar",
+    schaar: "papier",
+    papier: "steen"
+} 
+
+//main game function
  
-steenBTN.addEventListener('click', function(event) { 
+function rungame(event) {
     humanChoice = event.target.id;
     humanOutput.innerHTML = humanChoice
-})
-papierBTN.addEventListener('click', function(event) { 
-    humanChoice = event.target.id;
-    humanOutput.innerHTML = humanChoice
-})
-schaarBTN.addEventListener('click', function(event) { 
-    humanChoice = event.target.id;
-    humanOutput.innerHTML = humanChoice
-})
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    if ( randomNumber === 1 ) {
+        computerChoice = 'steen';
+    }
+
+    if ( randomNumber === 2 ) {
+        computerChoice = 'papier';
+    }
+
+    if ( randomNumber === 3 ) {
+        computerChoice = 'schaar';
+    }
+    computerOutput.innerHTML = computerChoice;
+
+    if (humanChoice === computerChoice){
+        result.innerHTML = "It's a Tie!"
+    }
+    else if (wins[humanChoice] === computerChoice) {
+        result.innerHTML = "You WIN!"
+    }
+    else { 
+        result.innerHTML = "You LOSE! 😂🤡"
+    }
+
+}
+// click events
+
+steenBTN.addEventListener('click', rungame)
+papierBTN.addEventListener('click', rungame)
+schaarBTN.addEventListener('click', rungame)
